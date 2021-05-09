8c33af40ba6cc2824b9f4acddfc55e0e6dbd6ef2
Ragnsan <ragnsanfv@gmail.com>
2021-05-09 12:25:35 +0200

    gjort cards om til lysere versjoner for å vise maten mer

--- components/1_Small/Cards/ImageCards.js
+++ components/1_Small/Cards/ImageCards.js
@@ -3,20 +3,22 @@ import { Bilde } from "../Base";
 
 export const BgImageCardLarge = ({ src, text, h }) => {
   return (
-    <div
-      class={`relative z-1 h-340 lg:h-650 dimmer-33 dimmer0  flex items-end p-24 w-100 stHover`}
-    >
-      <Link href="/meny">
-        <a>
-          <Bilde
-            src={src}
-            alt="Paneng curry"
-            layout="fill"
-            c="z-0 object-cover dimmer-50"
-          />
-        </a>
-      </Link>
-      <div class="relative z-1">{text}</div>
+    <div class="bg-gradient-to-t from-black to-white z-200">
+      <div
+        class={`relative z-1 h-340 lg:h-650 imgGradient dimmer-75 dimmer-100 flex items-end p-24 w-100 `}
+      >
+        <Link href="/meny">
+          <a>
+            <Bilde
+              src={src}
+              alt="Paneng curry"
+              layout="fill"
+              c="object-cover"
+            />
+          </a>
+        </Link>
+        <div class="relative z-1">{text}</div>
+      </div>
     </div>
   );
 };
@@ -24,7 +26,7 @@ export const BgImageCardLarge = ({ src, text, h }) => {
 export const BgImageCardSmall = ({ src, text, h }) => {
   return (
     <div
-      class={`relative z-1 h-340 dimmer-33 dimmer0  flex items-end p-24 w-100`}
+      class={`relative z-1 h-340 dimmer-75 dimmer-100 flex items-end p-24 w-100`}
     >
       <Link href="/meny">
         <a>
@@ -32,7 +34,7 @@ export const BgImageCardSmall = ({ src, text, h }) => {
             src={src}
             alt="Paneng curry"
             layout="fill"
-            c="z-0 object-cover dimmer-50"
+            c="z-0 object-cover"
           />
         </a>
       </Link>
--- components/3_Pages/0_Home/3_HomeAbout.js
+++ components/3_Pages/0_Home/3_HomeAbout.js
@@ -6,7 +6,7 @@ export const HomeAbout = () => {
     <Section160>
       <Layout>
         <Grid>
-          <GridItem c="col-span-4 md:col-span-8 lg:col-span-10">
+          <GridItem c="col-span-4 md:col-span-8 lg:col-span-12">
             <div class="mb-56">
               <h1 class="text-h5">THAIRESTAURANT I OSLO</h1>
               <h2 class="text-dark">
@@ -16,14 +16,14 @@ export const HomeAbout = () => {
             </div>
           </GridItem>
           <GridItem c="col-span-4 md:col-span-8 lg:col-span-12">
-            <div className="relative h-380 dimmer-33 dimmer0 text-light flex p-24 items-end">
+            <div className="relative h-380 dimmer-50 dimmer-100 text-light flex p-24 items-end">
               <Link href="om-oss">
                 <a className="textDeco">
                   <Bilde
                     src="demoside-thairestaurant/thai-kitchen_zm5ttz.jpg"
                     alt="Thailandsk kokk som lager wok"
                     layout="fill"
-                    c="z-0 object-cover hover:dimmer-0 dimmer-33"
+                    c="object-cover"
                   />
                   <div class="z-1 relative">
                     <h3 class="text-light mb-8">VÅR HISTORIE</h3>
